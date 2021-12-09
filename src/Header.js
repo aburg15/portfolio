4011bfc108f22b6b417c3b6e442ef0745a04e1fb
Adam Burgess <aburg1515@gmail.com>
2021-12-09 08:26:46 -0700

    Add Navbar

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import './Header.css'
+
+
+const Header = () => {
+  return (
+    <section className='header'>
+      <Navbar />
+    </section>
+  )
+
+}
+
+export default Header
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <section className='navbar'>
+      <a href='/' className='navbar-item'>Home</a>
+      <a href='/about' className='navbar-item'>About</a>
+      <a href='/projects' className='navbar-item'>Projects</a>
+      <a href='/contact' className='navbar-item'>Contact</a>
+    </section>
+  )
+
+
+}
+
+export default Navbar;
