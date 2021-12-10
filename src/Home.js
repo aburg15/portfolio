7dc1e97072f43f5ad50cbd33b5cb819c8b2e9a47
Adam Burgess <aburg1515@gmail.com>
2021-12-09 20:16:50 -0700

    Add social media links

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import headshot from './assets/img.jpg';
-
+import Resume from './assets/resume.pdf';
 import './Home.css'
 
 const Home = () => {
@@ -8,8 +9,18 @@ const Home = () => {
     <section className='home'>
       <img src={headshot} alt='Adam headshot' className='headshot-img'/>
       <div className="about-me">
+        <p>HI THERE, I'M</p>
         <h1>ADAM BURGESS</h1>
         <p>A Front-End Developer passionate about creating interactive applications and experiences on the web.</p>
+        <div className='icons'>
+          <a href={Resume} rel="noreferrer" target="_blank" className="resume-link">Resumé</a>
+          <a href="https://github.com/aburg15" className="logo" target="_blank">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/burgess-adam/" className="logo"  target="_blank">
+            <FaLinkedin />
+          </a> 
+        </div>
       </div>
     </section>
 
