a56dfc5c81e43962ebac8e766919ef303b5ee0b4
Adam Burgess <aburg1515@gmail.com>
2021-12-13 16:26:18 -0700

    Add link syntax

--- src/About.js
+++ src/About.js
@@ -6,7 +6,11 @@ const About = () => {
     <div className="about-me">
       <h1 className="about-me-header">ABOUT ME</h1>
       <p className="about-me-text">I am a front end software developer and former CPA with six years of experience in public accounting.  
+        <br/>
+        <br/>
         I recently decided to switch careers and pursue my passion for software development. I specialize in JavaScript and React. I also have experience working with HTML, CSS, Git and other front end development tools.  
+        <br/>
+        <br/>
         I am looking to further develop my skills as a junior developer by joining a team where I will be able to learn and provide value. If you think I would be a good fit for your company, please reach out!</p>
     </div>
 
