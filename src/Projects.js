e42e152b91141d58d177309ecf30e737b8a11e94
Adam Burgess <aburg1515@gmail.com>
2021-12-26 12:08:59 -0700

    Update project images

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,7 @@
 import React from "react";
-import RTimg from '../src/assets/rt.png'
-import TTimg from '../src/assets/tt.png'
-import FLimg from '../src/assets/FL.png'
+import RTgif from '../src/assets/rt.gif'
+import TTgif from '../src/assets/ttgif.gif'
+import FLgif from '../src/assets/FL.gif'
 import './Projects.css'
 
 const Projects = ( {theme} ) => {
@@ -9,7 +9,7 @@ const Projects = ( {theme} ) => {
     <div className="projects">
       <h1 className="projects-header">PROJECTS</h1>
         <div className="project-one">
-          <img className={theme === 'light' ? 'project-img' : 'project-img-dark'} alt="RT project" src={RTimg}/>
+          <img className={theme === 'light' ? 'project-img' : 'project-img-dark'} alt="RT project" src={RTgif}/>
           <section className="project-one-text">
             <h2>RANCID TOMATILLOS </h2>
             <div className="project-links">
@@ -28,12 +28,10 @@ const Projects = ( {theme} ) => {
           </section>
         </div>
         <div className="project-one">
-          <img className={theme === 'light' ? 'project-img' : 'project-img-dark'} alt="TT project" src={TTimg}/>
+          <img className={theme === 'light' ? 'project-img' : 'project-img-dark'} alt="TT project" src={TTgif}/>
           <section className="project-one-text">
             <h2>TRAVEL TRACKER</h2>
             <div className="project-links">
-              <a href="https://jkiernan12.github.io/rancid-tomatillo" className={theme === 'light' ? 'live-site' : 'live-site-dark'} target="_blank" rel="noreferrer">LIVE SITE</a>
-              <p className="dividor">|</p>
               <a href="https://github.com/aburg15/travel-tracker" className={theme === 'light' ? 'live-site' : 'live-site-dark'} target="_blank" rel="noreferrer"> GITHUB</a>
             </div>
             <p>Travel Tracker! is an application that allows users to view their trips. Upon logging in to the webpage, a user will be able to view their prior trips and future trips. Trips can be filtered down to pending trips and approved trips if they choose to do so. In addition to viewing existing trip information, a user has the option to post a new trip or receive a price quote based on the trip information entered by the user.
@@ -44,12 +42,10 @@ const Projects = ( {theme} ) => {
           </section>
         </div>
         <div className="project-one">
-          <img className={theme === 'light' ? 'project-img' : 'project-img-dark'} alt="TT project" src={FLimg}/>
+          <img className={theme === 'light' ? 'project-img' : 'project-img-dark'} alt="TT project" src={FLgif}/>
           <section className="project-one-text">
             <h2>FIT-LIT</h2>
             <div className="project-links">
-              <a href="https://jkiernan12.github.io/rancid-tomatillo" className={theme === 'light' ? 'live-site' : 'live-site-dark'} target="_blank" rel="noreferrer">LIVE SITE</a>
-              <p className="dividor">|</p>
               <a href="https://github.com/aburg15/fitlit-starter-kit" className={theme === 'light' ? 'live-site' : 'live-site-dark'} target="_blank" rel="noreferrer"> GITHUB</a>
             </div>
             <p>FitLit is an application that shows a user's steps, hydration, and sleep data. The data is displayed using charts which show the user's data compared to the overall community.
